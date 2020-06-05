6c3c9826781b23e7e32d37fa8ad52d70a958167e
amitjpr <a.amitjpr@gmail.com>
2020-06-05 18:38:27 +0530

    Public Reactive Properties Example

---
+++ force-app/main/default/lwc/meetingRooms/meetingRooms.js
@@ -0,0 +1,17 @@
+import { LightningElement } from 'lwc';
+
+export default class MeetingRooms extends LightningElement {
+
+    meetingRoomsInfo = [
+        {roomName: 'A-01',roomCapacity:'12'},
+        {roomName: 'A-02',roomCapacity:'16'},
+        {roomName: 'A-03',roomCapacity:'10'},
+        {roomName: 'A-04',roomCapacity:'8'},
+        {roomName: 'A-05',roomCapacity:'4'},
+        {roomName: 'A-06',roomCapacity:'2'},
+        {roomName: 'A-07',roomCapacity:'6'},
+        {roomName: 'A-08',roomCapacity:'20'},
+        {roomName: 'A-09',roomCapacity:'5'},
+        {roomName: 'A-10',roomCapacity:'15'}
+    ]
+}
