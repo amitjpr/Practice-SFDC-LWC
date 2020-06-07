83ebbd49b329ca0201eee47655165bfc32d945d9
amitjpr <a.amitjpr@gmail.com>
2020-06-07 19:28:40 +0530

    Communication between independent components

--- force-app/main/default/lwc/childToParentEventExample/childToParentEventExample.js
+++ force-app/main/default/lwc/childToParentEventExample/childToParentEventExample.js
@@ -1,13 +1,21 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import {fireEvent} from 'c/pubsub';
+import {CurrentPageReference} from 'lightning/navigation';
 
 export default class ChildToParentEventExample extends LightningElement {
     
     @api meetingRoomInfo = {roomName:'A-01',roomCapacity:'12'};
 
+    @wire(CurrentPageReference) pageReference;
+
     childEventGenerationHandler(){
         console.log('---->button clicked');
         const childButtonClicked = new CustomEvent('childclick',{detail :this.meetingRoomInfo });
+        //fire event from child to parent
         this.dispatchEvent(childButtonClicked);
+
+        //fire event from cmp to any cmp like application event in Aura lightning cmp
+        fireEvent(this.pageReference,'pubsublwcchildclick',this.meetingRoomInfo);
     }
     
 }
---
+++ force-app/main/default/lwc/selectedMeetingRoom/selectedMeetingRoom.js
@@ -0,0 +1,22 @@
+import { LightningElement ,track, wire} from 'lwc';
+import {registerListener, unregisterAllListeners} from 'c/pubsub';
+import {CurrentPageReference} from 'lightning/navigation';
+
+export default class SelectedMeetingRoom extends LightningElement {
+    @track selectedMeetingRoom = {};
+
+    @wire(CurrentPageReference) pageRef;
+
+    connectedCallback(){
+        registerListener('pubsublwcchildclick',this.onMeetingRoomSelectHandler,this);
+    }
+
+    disconnectedCallback(){
+        unregisterAllListeners(this);
+    }
+
+    onMeetingRoomSelectHandler(payload){
+        this.selectedMeetingRoom = payload;
+    }
+
+}
